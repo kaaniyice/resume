507197da5f99760ff2d70a161501945adeebbbc5
kannice <kaan35iyice@gmail.com>
2023-08-22 15:37:27 +0300

    feat: create skill model

--- core/admin.py
+++ core/admin.py
@@ -9,7 +9,6 @@ class GeneralSettingAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'description', 'parameter', 'updated_date', 'created_date']
     search_fields = ['name', 'description', 'parameter']
     list_editable = ['description', 'parameter']
-
     class Meta:
         model = GeneralSetting
 
@@ -19,6 +18,13 @@ class ImageSettingAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'description', 'file', 'updated_date', 'created_date']
     search_fields = ['name', 'description', 'file']
     list_editable = ['description', 'file']
-
     class Meta:
         model = ImageSetting
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order', 'name', 'percentage', 'updated_date', 'created_date']
+    search_fields = ['name']
+    list_editable = ['order', 'name', 'percentage']
+    class Meta:
+        model = Skill
